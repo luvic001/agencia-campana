ea0abda255f968740ed117ef60c85c35c5b68994
lucasvictor <lucas.v0697@gmail.com>
2022-02-23 17:07:06 -0300

    Quem Somos

--- modules/page/home/Content.php
+++ modules/page/home/Content.php
@@ -4,3 +4,4 @@ if (!defined('PATH')) exit;
 
 get_modules('Webdoor', 'page/home');
 get_modules('Section-marketing', 'page/home');
+get_modules('Section-quemsomos', 'page/home');
---
+++ modules/page/home/Section-quemsomos.php
@@ -0,0 +1,62 @@
+<?php
+
+if (!defined('PATH')) exit;
+$base = 'section-quemsomos';
+$base_format = '%s-%s';
+
+$section = (object) [
+  'title' => get_field(sprintf($base_format, $base, 'title')),
+  'subtitle' => get_field(sprintf($base_format, $base, 'subtitle')),
+  'content' => get_field(sprintf($base_format, $base, 'content')),
+  'cards' => get_field(sprintf($base_format, $base, 'cards'))
+];
+
+?>
+
+<section class="site-sections">
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-lg-5 col-xl-4">
+        <div class="section-title"> 
+          <h2><?= $section->title ?></h2>
+          <p><?= $section->subtitle ?></p>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-7 offset-xl-1 mb-4 mb-md-5">
+        <div class="editor-content mt-0 mt-md-4">
+          <?= $section->content ?>
+        </div>
+      </div>
+
+      <?php if ($section->cards): ?>
+        <div class="col-12">
+          <div class="section-cards">
+            <ul class="row">
+              <?php 
+                foreach ($section->cards as $card): 
+                  $image = $card[sprintf($base_format, $base, 'cards-image')];
+                  $content = $card[sprintf($base_format, $base, 'cards-content')];
+              ?>
+                  <li class="col-md-6 col-lg-4">
+                    <div class="card-content">
+                      <?php if ($image): ?>
+                        <figure>
+                          <img src="<?= wp_get_image($image) ?>" alt="">
+                        </figure>
+                      <?php endif; ?>
+                      <div class="editor-content">
+                        <p class="mb-0"><?= $content ?></p>
+                      </div>
+                    </div>
+                  </li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+        </div>
+      <?php endif; ?>
+      
+    </div>
+  </div>
+
+</section>
