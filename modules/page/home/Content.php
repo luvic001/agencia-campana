88ad717124ab32328f7c604e389ebdb6bdd16e19
lucasvictor <lucas.v0697@gmail.com>
2022-03-03 09:52:06 -0300

    Section CTA (Infoproduto)

--- modules/page/home/Content.php
+++ modules/page/home/Content.php
@@ -5,3 +5,5 @@ if (!defined('PATH')) exit;
 get_modules('Webdoor', 'page/home');
 get_modules('Section-marketing', 'page/home');
 get_modules('Section-quemsomos', 'page/home');
+get_modules('Section-oqfazemos', 'page/home');
+get_modules('Section-cta', 'page/home');
